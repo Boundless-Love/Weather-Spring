d25f9df40f3853ebb3009bb0d8fd51ccd870bc2e
Zhang <64055466+Zhangchuhan123@users.noreply.github.com>
2020-06-03 19:10:47 +0800

    提交Weather2

--- src/main/java/cn/edu/scujcc/dao/WeatherRepository.java
+++ src/main/java/cn/edu/scujcc/dao/WeatherRepository.java
@@ -1,5 +1,16 @@
 package cn.edu.scujcc.dao;
 
-public class WeatherRepository {
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import cn.edu.scujcc.model.Weather;
+
+@Repository
+public interface WeatherRepository extends MongoRepository<Weather,String>{
+	public List<Weather> findByCity(String city);
+	public List<Weather> findByDate(String date);
+	public Weather findByCityAndDate(String city,String date);
 
 }
